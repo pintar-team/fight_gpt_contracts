08908143d4160352b46b49d147d9034e954b597b
Hicaru <hicc0rus@gmail.com>
2023-12-07 11:30:25 +0700

    added ERC20 token

--- Token/ERC721.sol
+++ Token/ERC721.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "../@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../@openzeppelin/contracts/access/AccessControl.sol";
 import "../@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "../@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import "../@rarible/contracts/impl/RoyaltiesV2Impl.sol";
 import "../@rarible/contracts/LibPart.sol";
---
+++ tests/presale.py
@@ -0,0 +1,38 @@
+field_contributions = {}
+field_claim_state = {}
+field_total_contribution = 0
+field_start_block = 0
+field_end_block = 0
+
+field_started = False
+field_finished = False
+field_finished_fallback = False
+
+
+def contribute():
+    pass
+
+def claim():
+    pass
+
+def finishSale():
+    pass
+
+def fallback_token_refund():
+    pass
+
+def fallback_refund():
+    pass
+
+def initiate_sale(start: int, duration: int):
+    pass
+
+def initiate_sale_now(duration: int):
+    pass
+
+def initiate_sale_in(delay: int, duration: int):
+    start = builtin badd b delay
+  # endb = builtin badd start duration;
+  # StartSale start endb
+    pass
+
