1a10fc678324fce74dc86d10bc7aa2e3275d4d5b
hicaru <lich666black@gmail.com>
2023-12-10 09:35:03 +0500

    added ERC20 deploy

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,6 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const ERC20Token = artifacts.require("ERC20Token");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(ERC20Token);
 };
+
