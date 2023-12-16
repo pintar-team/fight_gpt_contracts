a145c1cabc657531b45ee8e8ef7d471561368f79
Hicaru <hicc0rus@gmail.com>
2023-12-16 12:41:31 +0700

    added remove effect from list

--- contracts/Effect.sol
+++ contracts/Effect.sol
@@ -2,10 +2,12 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
 import "./Item.sol";
 import "./Char.sol";
 
-contract Effect {
+contract Effect is AccessControl {
     using SafeCast for uint256;
 
     struct EffectData {
@@ -13,7 +15,8 @@ contract Effect {
         uint8 number_effects;
     }
 
-    address public immutable owner;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     HeroesGPT public char_token;
     Item public item_token;
@@ -22,19 +25,15 @@ contract Effect {
     mapping(uint256 => EffectData) public effects;
     // charID >> itemIDs
     mapping(uint256 => uint256[]) public char_effect;
-    // itemOwner >> itemID
-    mapping(address => uint256) public items_owner;
+    // itemID >> itemOwner
+    mapping(uint256 => address) public items_owner;
 
     event ItemContractChanged(Item oldContract, Item newContract);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "sender is not owner");
-        _;
-    }
 
     modifier onlyNotContract() {
         uint32 size;
         address _addr = msg.sender;
+        require(_addr != address(0), "sender is zero");
         assembly {
             size := extcodesize(_addr)
         }
@@ -45,37 +44,41 @@ contract Effect {
     constructor(address _item_contract, address _char_token) {
         item_token = Item(_item_contract);
         char_token = HeroesGPT(_char_token);
-        owner = msg.sender;
+        _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    function setItemContract(address _new_item_contract) external onlyOwner {
-        Item new_tem_token = Item(_new_item_contract);
-        emit ItemContractChanged(item_token, new_tem_token);
-
-        item_token = new_tem_token;
-    }
-
-    function takeThePill(uint256 _charID, uint256 _itemID)
-        external
-        onlyNotContract
-    {
+    function takeThePill(
+        uint256 _charID,
+        uint256 _itemID
+    ) external onlyNotContract {
         require(
             char_token.ownerOf(_charID) == msg.sender,
             "sender is not char owner"
         );
 
         item_token.transferFrom(msg.sender, address(this), _itemID);
-
-        items_owner[msg.sender] = _itemID;
+        items_owner[_itemID] = msg.sender;
         char_effect[_charID].push(_itemID);
     }
 
-    // can call only fight contract!s
+    function getBackItem(uint256 _itemID, uint256 _charID) external onlyNotContract {
+        require(items_owner[_itemID] == msg.sender, "Sender is not owner");
+        require(
+            char_token.ownerOf(_charID) == msg.sender,
+            "sender is not char owner"
+        );
+
+        item_token.transferFrom(address(this), msg.sender, _itemID);
+        removeEffect(_charID, _itemID);
+        delete items_owner[_itemID];
+    }
+
+    // can call only miner!
     function batchMint(
         address _to,
         uint64[] memory _effects,
         uint8[] memory _number_effects
-    ) external {
+    ) onlyRole(MINTER_ROLE) external {
         require(
             _effects.length == _number_effects.length,
             "Invalid input params"
@@ -96,12 +99,23 @@ contract Effect {
         }
     }
 
-    function getEffect(uint256 _tokenID)
-        public
-        view
-        returns (uint8 _effects, uint64 _effectId)
-    {
+    function getEffect(
+        uint256 _tokenID
+    ) public view returns (uint8 _effects, uint64 _effectId) {
         _effects = effects[_tokenID].number_effects;
         _effectId = effects[_tokenID].effectsId;
+    }
+
+    function removeEffect(uint256 _charID, uint256 _itemID) internal {
+        uint256[] storage effects_list = char_effect[_charID];
+        uint256 length = effects_list.length;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (effects_list[i] == _itemID) {
+                effects_list[i] = effects_list[length - 1];
+                effects_list.pop();
+                break;
+            }
+        }
     }
 }
