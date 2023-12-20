0e9927b676699bfa89b1e8a8944353502254b625
Hicaru <hicc0rus@gmail.com>
2023-12-20 16:45:52 +0700

    added stake unstake

--- contracts/Lobby.sol
+++ contracts/Lobby.sol
@@ -5,21 +5,24 @@ import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import "./Effect.sol";
 import "./Char.sol";
+import "./ERC20.sol";
 
 contract Lobby {
     using SafeCast for uint256;
 
     bool public pause = false;
+    uint256 public min_stake = 100;
 
     Effect public contract_effects;
     HeroesGPT public contract_char_token;
+    ERC20Token public contract_token;
 
     // queue for char, wait for fight
     uint256[20] public queue;
     // owner >> char_id
     mapping(address => uint256) public char_owners;
-    // char >> stake_amount
-    mapping(uint256 => uint256) public char_stake_amount;
+    // owner >> stake_amount
+    mapping(address => uint256) public stake_amount;
 
     modifier onlyNotContract() {
         uint32 size;
@@ -32,7 +35,25 @@ contract Lobby {
         _;
     }
 
-    function addChat(uint256 _charID) external {
-        contract_char_token.transferFrom(address(this), msg.sender, _charID);
+    function stake(uint256 _amount) external {
+        contract_token.transferFrom(msg.sender, address(this), _amount);
+        
+        stake_amount[msg.sender] += _amount;
+        // TODO: emit event.
+    }
+
+    function unstake(uint256 _amount) external {
+        require(stake_amount[msg.sender] <= _amount, "invalid amount");
+
+        contract_token.transferFrom(address(this), msg.sender, _amount);
+        stake_amount[msg.sender] -= _amount;
+        // TODO: emit event.
+    }
+
+    function add(uint256 _charID, uint256 _stake_amount) external {
+        require(!pause, "contract on pause");
+        require(_stake_amount >= min_stake, "invalid stake amount");
+
+        contract_char_token.transferFrom(msg.sender, address(this), _charID);
     }
 }
