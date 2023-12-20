53f5a2139987849104838907428c24e1853ad20e
Hicaru <hicc0rus@gmail.com>
2023-12-20 16:13:31 +0700

    generated lobby contract

---
+++ contracts/Lobby.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+import "./Effect.sol";
+import "./Char.sol";
+
+contract Lobby {}
