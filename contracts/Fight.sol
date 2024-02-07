341a111f9a405453d6dd66ebb2534970e3222b89
Hicaru <hicc0rus@gmail.com>
2024-02-07 18:47:18 +0700

    added tests methods for fights

--- contracts/Fight.sol
+++ contracts/Fight.sol
@@ -17,6 +17,7 @@ contract Fight {
 
     uint256 public waiting;
     uint256 public total_fights = 0;
+    uint8 public fee = 0;
 
     mapping(uint256 => uint256[2]) public fights;
     mapping(uint256 => uint8) public rounds;
@@ -29,23 +30,68 @@ contract Fight {
     ) external {
         // TODO: add checers;
         add(_charID, _rounds, _stake_amount);
+        // TODO: add events;
     }
 
-    function commit(uint256 _fightid) external {
-        // TODO: add checers;
+    function commit(uint256 _fightid, uint256 _wonid) external {
+        uint256[2] memory lobby = fights[_fightid];
+        uint256 winnerid = (_wonid == lobby[0]) ? lobby[0] : lobby[1];
+        uint256 loserid = (_wonid == lobby[1]) ? lobby[1] : lobby[0];
+        uint256 winstake = stakes[winnerid];
+        uint256 losestake = stakes[loserid];
+
+        (uint256 potentialGain, uint256 platformFee, uint256 winnerGain) = redistributeStakes(winstake, losestake);
+
+        stakes[loserid] -= potentialGain;
+        stakes[winnerid] += winnerGain;
+        // TODO: send fee for commiter!
+
     }
 
     function add(uint256 _id, uint8 _rounds, uint256 _stake) internal {
         if (waiting == 0) {
             waiting = _id;
         } else {
-            uint256 opponent = waiting;
-            total_fights++;
-
-            fights[total_fights] = [_id, opponent];
+            startFight(_id);
         }
 
         rounds[_id] = _rounds;
         stakes[_id] = _stake;
+    }
+
+    function startFight(uint256 _id) internal {
+        total_fights++;
+
+        fights[total_fights] = [_id, waiting];
+        waiting = 0;
+    }
+
+    function updateRounds(uint256 _id) internal {
+        if (rounds[_id] == 1) {
+            pop();
+        } else {
+            uint8 newRounds = rounds[_id] - 1;
+            add(_id, newRounds, stakes[_id]);
+        }
+    }
+
+    function pop() internal {
+        // TODO: here transfer tokens back.
+    }
+
+    function redistributeStakes(uint256 _winner, uint256 _loser) public view returns (uint256, uint256, uint256) {
+        uint256 potentialGain = min(_winner, _loser);
+        uint256 platformFee = (potentialGain * fee) / 100;
+        uint256 winnerGain = potentialGain - platformFee;
+
+        return (potentialGain, platformFee, winnerGain);
+    }
+
+    function min(uint256 a, uint256 b) private pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    function max(uint256 a, uint256 b) private pure returns (uint256) {
+        return a > b ? a : b;
     }
 }
