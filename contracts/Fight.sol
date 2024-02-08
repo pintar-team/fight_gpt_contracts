033326066dfbd36a741ddc5fd8c1e853e1533825
Hicaru <hicc0rus@gmail.com>
2024-02-08 12:54:35 +0700

    added testing fight algo

--- contracts/Fight.sol
+++ contracts/Fight.sol
@@ -13,8 +13,8 @@ contract Fight {
     using SafeCast for uint256;
 
     struct Lobby {
-        uint256 token0;
-        uint256 token1;
+        uint256 id0;
+        uint256 id1;
     }
 
     Effect public contract_effects;
@@ -25,15 +25,91 @@ contract Fight {
     uint256 public total_fights = 0;
     uint8 public fee = 0;
 
-    mapping(uint256 => uint256[2]) public fights;
+    mapping(uint256 => Lobby) public fights;
     mapping(uint256 => uint8) public rounds;
     mapping(uint256 => uint256) public stakes;
 
-    function join(uint256 _id, uint256 _stake, uint8 _rounds) external {
+    function join(
+        uint256 _id,
+        uint256 _stake,
+        uint8 _rounds
+    ) external {
         /// TODO: add checerks
+        // TODO: add transfers.
+        add(_id, _stake, _rounds);
     }
 
-    function redistributeStakes(uint256 _winner, uint256 _loser) public view returns (uint256, uint256, uint256) {
+    function commit(uint256 _fightid, uint256 _wonid) external {
+        /// TODO: add checerks.
+        Lobby memory lobby = fights[_fightid];
+        uint256 winnerid = (_wonid == lobby.id0) ? lobby.id0 : lobby.id1;
+        uint256 loserid = (_wonid == lobby.id1) ? lobby.id1 : lobby.id0;
+        uint256 winstake = stakes[winnerid];
+        uint256 losestake = stakes[loserid];
+
+        (
+            uint256 potentialGain,
+            uint256 platformFee,
+            uint256 winnerGain
+        ) = redistributeStakes(winstake, losestake);
+
+        stakes[loserid] -= potentialGain;
+        stakes[winnerid] += winnerGain;
+    }
+
+    function addWaitlist(uint256 _id) internal {
+        waiting.add(_id);
+        // TODO: add Events.
+    }
+
+    function startFight(uint256 _id, uint256 _opponent) internal {
+        total_fights++;
+        fights[total_fights] = Lobby(_id, _opponent);
+        // TODO: add Events.
+    }
+
+     function updateRounds(uint256 _id) internal {
+        if (rounds[_id] == 1) {
+            pop(_id);
+        } else {
+            uint8 newRounds = rounds[_id] - 1;
+            add(_id, stakes[_id], newRounds);
+        }
+    }
+
+    function pop(uint256 _id) internal {
+        // TODO: here transfer tokens back.
+        rounds[_id] = 0;
+        stakes[_id] = 0;
+    }
+
+    function add(
+        uint256 _id,
+        uint256 _stake,
+        uint8 _rounds
+    ) internal {
+        if (waiting.hasEmpty()) {
+            addWaitlist(_id);
+        } else {
+            uint256 opponent = waiting.get(0);
+
+            waiting.pop();
+            startFight(_id, opponent);
+        }
+
+        stakes[_id] = _stake;
+        rounds[_id] = _rounds;
+    }
+
+    function redistributeStakes(uint256 _winner, uint256 _loser)
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
         uint256 potentialGain = min(_winner, _loser);
         uint256 platformFee = (potentialGain * fee) / 100;
         uint256 winnerGain = potentialGain - platformFee;
