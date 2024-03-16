d765fb5e11980e2b134d232f82c821d8a448a589
hicaru <lich666black@gmail.com>
2024-03-16 10:35:23 +0500

    added test for whitelist remove method

--- test/WaitList.ts
+++ test/WaitList.ts
@@ -2,63 +2,85 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-describe("WaitList contract", function () {
-    let WaitList: any;
-    let waitList: any;
-    let accounts: HardhatEthersSigner[];
+describe("WaitList contract", function() {
+  let WaitList: any;
+  let waitList: any;
+  let accounts: HardhatEthersSigner[];
 
-    beforeEach(async function () {
-        WaitList = await ethers.getContractFactory("WaitList");
-        accounts = await ethers.getSigners();
-        waitList = await WaitList.deploy();
+  beforeEach(async function() {
+    WaitList = await ethers.getContractFactory("WaitList");
+    accounts = await ethers.getSigners();
+    waitList = await WaitList.deploy();
+  });
+
+  describe("Check add and remove elements", async function() {
+    it("add and pop", async function() {
+      expect(await waitList.hasEmpty()).to.equal(true);
+      expect(await waitList.hasSpace()).to.equal(true);
+      await waitList.add(1);
+      expect(await waitList.hasEmpty()).to.equal(false);
+      expect(await waitList.hasSpace()).to.equal(true);
+      await waitList.add(2);
+      await expect(waitList.add(2)).to.be.reverted;
+      await waitList.add(3);
+      expect(await waitList.hasSpace()).to.equal(true);
+      await waitList.add(4);
+      expect(await waitList.hasSpace()).to.equal(true);
+      await waitList.add(5);
+
+      await expect(waitList.add(6)).to.be.reverted;
+      expect(await waitList.count()).to.equal(5n);
+      expect(await waitList.waiting(0)).to.equal(1n);
+      expect(await waitList.waiting(1)).to.equal(2n);
+      expect(await waitList.waiting(2)).to.equal(3n);
+      expect(await waitList.waiting(3)).to.equal(4n);
+      expect(await waitList.waiting(4)).to.equal(5n);
+
+      expect(await waitList.hasSpace()).to.equal(false);
+      await waitList.pop();
+      expect(await waitList.hasSpace()).to.equal(true);
+
+      expect(await waitList.waiting(4)).to.equal(0n);
+      expect(await waitList.waiting(0)).to.equal(2n);
+
+      await waitList.pop();
+
+      expect(await waitList.waiting(4)).to.equal(0n);
+      expect(await waitList.waiting(3)).to.equal(0n);
+      expect(await waitList.waiting(0)).to.equal(3n);
+
+      await waitList.add(1);
+
+      expect(await waitList.waiting(3)).to.equal(1n);
+      expect(await waitList.waiting(4)).to.equal(0n);
+
+      await waitList.add(2);
+
+      expect(await waitList.waiting(3)).to.equal(1n);
+      expect(await waitList.waiting(4)).to.equal(2n);
     });
 
-    describe("Check add and remove elements", async function() {
-        it("add and pop", async function () {
+    it("add and remove", async function() {
+      expect(await waitList.hasEmpty()).to.equal(true);
+      expect(await waitList.hasSpace()).to.equal(true);
 
-          expect(await waitList.hasEmpty()).to.equal(true);
-          expect(await waitList.hasSpace()).to.equal(true);
-          await waitList.add(1);
-          expect(await waitList.hasEmpty()).to.equal(false);
-          expect(await waitList.hasSpace()).to.equal(true);
-          await waitList.add(2);
-          await expect(waitList.add(2)).to.be.reverted;
-          await waitList.add(3);
-          expect(await waitList.hasSpace()).to.equal(true);
-          await waitList.add(4);
-          expect(await waitList.hasSpace()).to.equal(true);
-          await waitList.add(5);
+      await waitList.add(1);
+      await waitList.add(2);
+      await waitList.add(3);
+      await waitList.add(4);
 
-          await expect(waitList.add(6)).to.be.reverted;
-          expect(await waitList.count()).to.equal(5n);
-          expect(await waitList.waiting(0)).to.equal(1n);
-          expect(await waitList.waiting(1)).to.equal(2n);
-          expect(await waitList.waiting(2)).to.equal(3n);
-          expect(await waitList.waiting(3)).to.equal(4n);
-          expect(await waitList.waiting(4)).to.equal(5n);
+      expect(await waitList.count()).to.equal(4n);
+      expect(await waitList.waiting(0)).to.equal(1n);
+      expect(await waitList.waiting(1)).to.equal(2n);
+      expect(await waitList.waiting(2)).to.equal(3n);
+      expect(await waitList.waiting(3)).to.equal(4n);
 
-          expect(await waitList.hasSpace()).to.equal(false);
-          await waitList.pop();
-          expect(await waitList.hasSpace()).to.equal(true);
+      await waitList.remove(3);
 
-          expect(await waitList.waiting(4)).to.equal(0n);
-          expect(await waitList.waiting(0)).to.equal(2n);
-
-          await waitList.pop();
-
-          expect(await waitList.waiting(4)).to.equal(0n);
-          expect(await waitList.waiting(3)).to.equal(0n);
-          expect(await waitList.waiting(0)).to.equal(3n);
-
-          await waitList.add(1);
-
-          expect(await waitList.waiting(3)).to.equal(1n);
-          expect(await waitList.waiting(4)).to.equal(0n);
-
-          await waitList.add(2);
-
-          expect(await waitList.waiting(3)).to.equal(1n);
-          expect(await waitList.waiting(4)).to.equal(2n);
-        });
+      expect(await waitList.count()).to.equal(3n);
+      expect(await waitList.waiting(0)).to.equal(1n);
+      expect(await waitList.waiting(1)).to.equal(2n);
+      expect(await waitList.waiting(2)).to.equal(4n);
     });
+  });
 });
