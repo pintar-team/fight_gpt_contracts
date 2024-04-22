9ebbe5308bf5a2a0b75ba34ca2657b798270756d
Rinat <rinat@horsefucker.org>
2024-04-22 08:47:19 +0300

    remake presale and added tests

--- contracts/TokenPreSale.sol
+++ contracts/TokenPreSale.sol
@@ -1,332 +1,178 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 contract TokenPreSale {
-    ERC20Token public immutable token;
+    using SafeERC20 for IERC20;
+    using SafeCast for uint256;
+
+    IERC20 public immutable token;
     address public immutable owner;
-    uint256 public immutable token_price;
-    uint256 public immutable target_maximum;
-    uint256 public immutable target_minimum;
-    uint256 public immutable cooldown;
+    uint256 public immutable tokenPrice;
+    uint256 public immutable targetMaximum;
+    uint256 public immutable targetMinimum;
+    uint256 public immutable cooldownPeriod;
     uint256 public immutable deadline;
 
-    // token_price * target_minimum
-    uint256 public min_contribution;
-    // token_price * target_maximum
-    uint256 public max_contribution;
+    uint256 public immutable minContribution;
+    uint256 public immutable maxContribution;
 
-    bool public started = false;
-    bool public finished = false;
-    bool public finished_fallback = false;
+    uint256 public startBlock;
+    uint256 public endBlock;
+    uint256 public cooldownBlock;
+    uint256 public deadlineBlock;
 
-    uint256 public total_contribution = 0;
-    uint256 public start_block = block.number;
-    uint256 public end_block = block.number;
-    uint256 public cooldown_block = block.number;
-    uint256 public deadline_block = block.number;
+    bool public started;
+    bool public finished;
+    bool public finishedFallback;
 
-    // address > BNB amount
     mapping(address => uint256) public contribution;
-    // address > maybe claimed
-    mapping(address => bool) public claim_state;
+    mapping(address => uint256) public claimState;
 
-    event SaleInitiated(
-        uint256 start_block,
-        uint256 end_block,
-        uint256 min_contribution,
-        uint256 max_contribution
-    );
-    event RefundContribution(uint256 amount, uint256 current_contribution);
-    event ContrubutionAdded(uint256 amount, uint256 new_contribution);
+    uint256 public totalContribution;
+
+    event SaleInitiated(uint256 startBlock, uint256 endBlock);
+    event ContributionAdded(uint256 amount, uint256 newContribution);
+    event Finished();
+    event Claimed();
+    event RefundContribution(uint256 amount);
     event TokenClaimed(uint256 amount);
     event RefundToken(uint256 amount);
     event FundSale(uint256 amount);
-    event Finished();
+    event TopUpBalance(uint256 tokens, uint256 amount);
+
+    modifier onlyWhenActive() {
+        require(
+            block.number >= startBlock && block.number < endBlock,
+            "Presale is not active"
+        );
+        _;
+    }
+
+    modifier onlyWhenFinished() {
+        require(finished, "Presale is not finished");
+        _;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    modifier throwNotActive() {
-        require(
-            block.number >= start_block && block.number < end_block,
-            "Presale is not active"
-        );
-        _;
-    }
-
-    modifier throwIfDeadline() {
-        bool isActive = (!finished && block.number < deadline_block);
-
-        require(isActive, "Presale is deadlines");
-        _;
-    }
-
-    modifier throwNotDeadline() {
-        require(block.number < deadline_block, "Presale is not deadline");
-        _;
-    }
-
-    modifier throwNotEnded() {
-        require(block.number < end_block, "Presale is not ended");
-        _;
-    }
-
-    modifier throwIfCooldown() {
-        require(block.number < cooldown_block, "Presale is cooldown");
-        _;
-    }
-
-    modifier throwNotFinished() {
-        require(finished, "Presale is not finished");
-        _;
-    }
-
-    modifier throwIfStarted() {
-        require(!started, "Presale is started");
-        _;
-    }
-
-    modifier throwIfFinished() {
-        require(!finished, "Presale is finished");
-        _;
-    }
-
-    modifier throwIfFinishedFallback() {
-        require(!finished_fallback, "Presale is finished fallback");
-        _;
-    }
-
-    modifier throwNotStarted() {
-        require(started, "Presale is not started");
-        _;
-    }
-
     constructor(
-        uint256 _token_price,
-        uint256 _target_maximum,
-        uint256 _target_minimum,
-        uint256 _cooldown,
+        address _token,
+        uint256 _tokenPrice,
+        uint256 _targetMaximum,
+        uint256 _targetMinimum,
+        uint256 _cooldownPeriod,
         uint256 _deadline
     ) {
-        token = new ERC20Token();
-        owner = msg.sender;
-        token_price = _token_price;
-        target_maximum = _target_maximum;
-        target_minimum = _target_minimum;
-        min_contribution = _token_price * _target_minimum;
-        max_contribution = _token_price * _target_maximum;
-        cooldown = _cooldown;
+        token = IERC20(_token);
+        tokenPrice = _tokenPrice;
+        targetMaximum = _targetMaximum;
+        targetMinimum = _targetMinimum;
+        cooldownPeriod = _cooldownPeriod;
         deadline = _deadline;
+        owner = msg.sender;
+
+        minContribution = tokenPrice * targetMinimum;
+        maxContribution = tokenPrice * targetMaximum;
+
+        startBlock = block.number;
+        endBlock = block.number;
+        cooldownBlock = block.number;
+        deadlineBlock = block.number;
     }
 
-    function contribute()
-        external
-        payable
-        throwNotStarted
-        throwNotActive
-        throwIfFinished
-    {
-        require(msg.sender != address(0), "Zero token address");
+    function contribute() external payable onlyWhenActive {
+        uint256 tokenAmount = msg.value / tokenPrice;
+        require(tokenAmount > 0, "Invalid contribution amount");
 
-        address _beneficiary = msg.sender;
-        uint256 _value = msg.value;
-        uint256 token_amount = _value / token_price;
+        uint256 contributionAmount = tokenAmount * tokenPrice;
+        uint256 refundAmount = msg.value - contributionAmount;
 
-        require(token_amount > 0, "invalid amount");
-
-        uint256 contribute_amount = token_amount * token_price;
-        uint256 current_contribution = contribution[_beneficiary];
-        uint256 new_contribution = current_contribution + contribute_amount;
-
-        require(new_contribution <= max_contribution, "Invalid amount");
-
-        if (_value > contribute_amount) {
-            uint256 refund_amount = _value - contribute_amount;
-            payable(msg.sender).transfer(refund_amount);
-            emit RefundContribution(refund_amount, new_contribution);
+        if (refundAmount > 0) {
+            payable(msg.sender).transfer(refundAmount);
+            emit RefundContribution(refundAmount);
         }
 
-        uint256 new_total_contribution = total_contribution + contribute_amount;
+        uint256 newContribution = contribution[msg.sender] + contributionAmount;
+        require(
+            newContribution <= maxContribution,
+            "Contribution exceeds maximum limit"
+        );
 
-        contribution[_beneficiary] = new_contribution;
-        total_contribution = new_total_contribution;
+        contribution[msg.sender] = newContribution;
+        totalContribution = totalContribution + contributionAmount;
 
-        emit ContrubutionAdded(_value, new_total_contribution);
+        emit ContributionAdded(msg.value, newContribution);
     }
 
-    function claim()
-        external
-        throwNotStarted
-        throwNotFinished
-        throwIfDeadline
-        throwIfCooldown
-    {
-        require(!claim_state[msg.sender], "Already claimed");
+    function claim() external onlyWhenFinished {
+        require(block.number < deadlineBlock, "Claim deadline passed");
+        require(block.number >= cooldownBlock, "Claiming is on cooldown");
+        require(claimState[msg.sender] == 0, "Already claimed");
 
-        uint256 current_contribution = contribution[msg.sender];
-        bool is_underlow = total_contribution < min_contribution;
-
-        if (is_underlow) {
-            payable(msg.sender).transfer(current_contribution);
-            emit RefundContribution(current_contribution, 0);
-        } else {
-            bool is_overflow = max_contribution < total_contribution;
-            uint256 real_contribution = this.getRealContribution(
-                is_overflow,
-                current_contribution
-            );
-            uint256 token_amount = real_contribution / token_price;
-            uint256 spend_amount = 0;
-
-            if (is_overflow) {
-                spend_amount = this.muldiv_up(
-                    current_contribution,
-                    max_contribution,
-                    total_contribution
-                );
-            } else {
-                spend_amount = token_amount * token_price;
-            }
-
-            if (current_contribution > spend_amount) {
-                uint256 refund_amount = current_contribution - spend_amount;
-                payable(msg.sender).transfer(refund_amount);
-                emit RefundContribution(refund_amount, current_contribution);
-            }
-
-            if (token_amount > 0) {
-                token.transfer(msg.sender, token_amount);
-                emit TokenClaimed(token_amount);
-            }
-        }
-
-        claim_state[msg.sender] = true;
+        claimState[msg.sender] = 1;
+        processClaim();
+        emit Claimed();
     }
 
-    function finishSale()
-        external
-        throwNotStarted
-        throwNotEnded
-        throwIfFinished
-        throwIfDeadline
-    {
-        bool is_overflow = max_contribution < total_contribution;
-        bool is_underlow = total_contribution < min_contribution;
+    function initiatePresale(uint256 duration) external onlyOwner {
+        require(!started, "Presale already started");
 
-        if (is_underlow) {
-            uint256 max_token_amount = max_contribution / token_price;
-            // TODO: replace to multisig wallet.
-            token.transfer(owner, max_token_amount);
-            emit RefundToken(max_token_amount);
-        } else {
-            uint256 fix_contribution = is_overflow
-                ? max_contribution
-                : total_contribution;
-            uint256 max_token_amount = max_contribution / token_price;
-            uint256 sold_token_amount = fix_contribution / token_price;
-            uint256 fund_amount = sold_token_amount / token_price;
-            uint256 refund_token_amount = max_token_amount - sold_token_amount;
-
-            if (fund_amount > 0) {
-                // TODO: replace to multisig wallet.
-                payable(owner).transfer(fund_amount);
-                emit FundSale(fund_amount);
-            }
-
-            if (refund_token_amount > 0) {
-                // TODO: replace to multisig wallet.
-                token.transfer(owner, refund_token_amount);
-                emit RefundToken(refund_token_amount);
-            }
-        }
-
-        cooldown_block = block.number + cooldown;
-        finished = true;
-        emit Finished();
-    }
-
-    function fallbackRefund()
-        external
-        throwNotStarted
-        throwNotDeadline
-        throwIfFinished
-    {
-        require(!claim_state[msg.sender], "Already claimed");
-        uint256 current_contribution = contribution[msg.sender];
-
-        if (current_contribution > 0) {
-            emit RefundContribution(current_contribution, 0);
-            claim_state[msg.sender] = true;
-            payable(msg.sender).transfer(current_contribution);
-        }
-    }
-
-    function initiateSale(uint256 duration) external onlyOwner throwIfStarted {
-        uint256 start = block.number + 1;
-        uint256 end = start + duration;
-
-        startSale(start, end);
-    }
-
-    function startSale(uint256 _startblock, uint256 _endblock) internal {
-        require(block.number < _startblock, "Invalid start block");
-        require(_startblock < _endblock, "Invalid end block");
+        startBlock = block.number + 1;
+        endBlock = startBlock + duration;
+        deadlineBlock = endBlock + deadline;
 
         started = true;
-        deadline_block = _endblock + deadline;
-        min_contribution = token_price * target_minimum;
-        max_contribution = token_price * target_maximum;
 
-        emit SaleInitiated(
-            _startblock,
-            _endblock,
-            min_contribution,
-            max_contribution
-        );
+        token.safeTransferFrom(msg.sender, address(this), targetMaximum);
+
+        emit SaleInitiated(startBlock, endBlock);
     }
 
-    function muldiv(
-        uint256 a,
-        uint256 b,
-        uint256 c
-    ) public pure returns (uint256) {
-        uint256 num = a * b;
-        uint256 hv = num / c;
+    function processClaim() private {
+        uint256 currentContribution = contribution[msg.sender];
 
-        return hv;
-    }
+        if (totalContribution < minContribution) {
+            payable(msg.sender).transfer(currentContribution);
+            emit RefundContribution(currentContribution);
+        } else {
+            uint256 realContribution;
+            if (totalContribution > maxContribution) {
+                realContribution =
+                    (currentContribution * maxContribution) /
+                    totalContribution;
+            } else {
+                realContribution = currentContribution;
+            }
 
-    function muldiv_up(
-        uint256 a,
-        uint256 b,
-        uint256 c
-    ) public pure returns (uint256) {
-        uint256 num = a * b;
-        uint256 num_p = num + c;
-        uint256 num_p_1 = num_p - 1;
-        uint256 hv = num_p_1 / c;
+            uint256 tokenAmount = realContribution / tokenPrice;
+            uint256 spentAmount;
+            if (totalContribution > maxContribution) {
+                spentAmount =
+                    (currentContribution * maxContribution) /
+                    totalContribution +
+                    1;
+            } else {
+                spentAmount = tokenAmount * tokenPrice;
+            }
 
-        return hv;
-    }
+            if (currentContribution > spentAmount) {
+                uint256 refundAmount = currentContribution - spentAmount;
+                payable(msg.sender).transfer(refundAmount);
+                emit RefundContribution(refundAmount);
+            }
 
-    function getRealContribution(bool is_overflow, uint256 current_contribution)
-        public
-        view
-        returns (uint256)
-    {
-        if (is_overflow) {
-            return
-                muldiv(
-                    current_contribution,
-                    max_contribution,
-                    total_contribution
-                );
+            if (tokenAmount > 0) {
+                token.safeTransfer(msg.sender, tokenAmount);
+                emit TokenClaimed(tokenAmount);
+            }
         }
-
-        return current_contribution;
     }
 }
--- test/PreSale.ts
+++ test/PreSale.ts
@@ -11,129 +11,65 @@ describe("TokenPreSale", function() {
   let addr2: HardhatEthersSigner;
   let addresses: HardhatEthersSigner[];
 
+  const tokenPrice = ethers.parseEther("0.1");
+  const targetMaximum = ethers.parseEther("1000");
+  const targetMinimum = ethers.parseEther("100");
+  const cooldownPeriod = 3600;
+  const deadline = 7200;
+
   beforeEach(async function() {
     [owner, addr1, addr2, ...addresses] = await ethers.getSigners();
 
+    const ERC20Token = await ethers.getContractFactory("ERC20Token");
     const TokenPreSale = await ethers.getContractFactory("TokenPreSale");
 
+    token = await ERC20Token.deploy();
+
     tokenPreSale = await TokenPreSale.deploy(
-      ethers.parseEther("0.01"), // token_price
-      1000, // target_maximum
-      100,  // target_minimum
-      100,  // cooldown
-      1000  // deadline
+      token.target,
+      tokenPrice,
+      targetMaximum,
+      targetMinimum,
+      cooldownPeriod,
+      deadline
     );
-
-    const tokenAddress = await tokenPreSale.token();
-
-    token = await ethers.getContractAt("IERC20", tokenAddress);
   });
 
-  it("Should initiate the sale correctly", async function() {
+  describe("initiatePresale", function() {
     const duration = 1000;
-    const currentBlockNumber = await ethers.provider.getBlockNumber();
 
-    await expect(tokenPreSale.initiateSale(duration))
-      .to.emit(tokenPreSale, "SaleInitiated")
-      .withArgs(
-        BigInt((currentBlockNumber) + 2), // 2 because the tx in current block + 1
-        BigInt((currentBlockNumber) + 1002), // 1002 because (current_block + 1) - duration
-        ethers.parseEther("1"), // min_contribution
-        ethers.parseEther("10")  // max_contribution
+    it("Should initiate the presale correctly", async function() {
+      await token.approve(tokenPreSale.target, targetMaximum);
+
+      const tx = await tokenPreSale.initiatePresale(duration);
+
+      expect(await tokenPreSale.started()).to.equal(true);
+      expect(await tokenPreSale.startBlock()).to.equal(tx.blockNumber + 1);
+      expect(await tokenPreSale.endBlock()).to.equal(tx.blockNumber + 1 + duration);
+      expect(await tokenPreSale.deadlineBlock()).to.equal(tx.blockNumber + 1 + duration + deadline);
+
+      await expect(tx)
+        .to.emit(tokenPreSale, "SaleInitiated")
+        .withArgs(
+          tx.blockNumber + 1,
+          tx.blockNumber + 1 + duration
+        );
+    });
+
+    it("Should fail if presale is already started", async function() {
+      await token.approve(tokenPreSale.target, targetMaximum);
+      await tokenPreSale.initiatePresale(duration);
+
+      await expect(tokenPreSale.initiatePresale(duration)).to.be.revertedWith(
+        "Presale already started"
       );
+    });
+
+    it("Should fail if caller is not the owner", async function() {
+      await expect(tokenPreSale.connect(addr1).initiatePresale(duration)).to.be.revertedWith(
+        "Not the owner"
+      );
+    });
   });
 
-  it("Should handle contributions from multiple addresses and distribute tokens correctly", async function() {
-    const duration = 1000;
-    await tokenPreSale.initiateSale(duration);
-
-    const contributions = [
-      ethers.parseEther("2"),
-      ethers.parseEther("3"),
-      ethers.parseEther("1.5"),
-      ethers.parseEther("4"),
-      ethers.parseEther("2.5"),
-      ethers.parseEther("1"),
-      ethers.parseEther("3.5"),
-      ethers.parseEther("2"),
-      ethers.parseEther("4.5"),
-      ethers.parseEther("1.5")
-    ];
-
-    for (let i = 0; i < 10; i++) {
-      await tokenPreSale.connect(addresses[i]).contribute({ value: contributions[i] });
-      const contributionAmount = await tokenPreSale.contribution(await addresses[i].getAddress());
-      expect(contributionAmount).to.equal(contributions[i]);
-    }
-
-    // await ethers.provider.send("evm_increaseTime", [1500]);
-    // await ethers.provider.send("evm_mine", []);
-
-    await mine(duration);
-
-    await tokenPreSale.connect(owner).finishSale();
-
-    for (let i = 0; i < 10; i++) {
-      const contribution = BigInt(contributions[i]);
-      const expectedTokens = contribution / ethers.parseEther("0.01");
-
-      await tokenPreSale.connect(addresses[i]).claim();
-
-      console.log(expectedTokens, await token.balanceOf(await addresses[i].getAddress()));
-
-      // expect(await token.balanceOf(await addresses[i].getAddress())).to.equal(expectedTokens);
-    }
-
-    // const ownerTokens = (await token.balanceOf(await owner.getAddress())).toString();
-    //
-    // console.log(ownerTokens);
-
-    // expect(ownerTokens).to.equal("2500");
-  });
-
-  // it("Should contribute and claim tokens", async function() {
-  //   const duration = 1000;
-  //   const currentBlockNumber = await ethers.provider.getBlockNumber();
-  //
-  //   await tokenPreSale.initiateSale(duration);
-  //
-  //   const contribution = ethers.parseEther("5");
-  //   await tokenPreSale.connect(addr1).contribute({ value: contribution });
-  //
-  //   expect(await tokenPreSale.contribution(addr1.getAddress())).to.equal(contribution);
-  //
-  //   console.log(await ethers.provider.getBlockNumber());
-  //
-  //   await expect(tokenPreSale.connect(addr1).claim())
-  //     .to.be.reverted;
-  //
-  //   // await ethers.provider.send("evm_increaseTime", [1500]);
-  //   // await ethers.provider.send("evm_mine", [100]);
-  //
-  //   await mine(100);
-  //
-  //   console.log(await ethers.provider.getBlockNumber());
-  //
-  //   // .to.emit(token, "Transfer")
-  //   // .withArgs(tokenPreSale.address, addr1.getAddress(), 500);
-  //
-  //   // expect(await token.balanceOf(addr1.getAddress())).to.equal(500);
-  // });
-
-  //
-  // it("Should finish the sale and distribute tokens", async function() {
-  //   await tokenPreSale.initiateSale(1000);
-  //
-  //   await tokenPreSale.connect(addr1).contribute({ value: ethers.parseEther("5") });
-  //   await tokenPreSale.connect(addr2).contribute({ value: ethers.parseEther("10") });
-  //
-  //   await ethers.provider.send("evm_increaseTime", [1001]);
-  //   await ethers.provider.send("evm_mine", []);
-  //
-  //   await expect(tokenPreSale.finishSale())
-  //     .to.emit(tokenPreSale, "Finished");
-  //
-  //   expect(await token.balanceOf(owner.getAddress())).to.equal(500);
-  //   expect(await ethers.provider.getBalance(tokenPreSale.address)).to.equal(0);
-  // });
 });
